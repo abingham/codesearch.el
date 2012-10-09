63318afe56fdc9d0e26da39eae8a2e6064bfc667
Austin Bingham <austin.bingham@gmail.com>
2012-10-09 20:25:44 +0200

    Added some documentation.

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ codesearch.el
 =============
 
 An emacs extension for using the codesearch sourcecode indexer.
+
+This extension allows you to use the codesearch code indexing
+system in emacs.
+
+For more details, see the project page at
+https://github.com/abingham/prosjekt.
+
+For more details on codesearch, see its project page at
+http://code.google.com/p/codesearch/
+
+Installation:
+
+Copy codesearch.el to some location in your emacs load path. Then add
+"(require 'codesearch)" to your emacs initialization (.emacs,
+init.el, or something).
+
+Example config:
+
+  (require 'codesearch)
--- codesearch.el
+++ codesearch.el
@@ -1,3 +1,62 @@
+;;; codesearch.el --- An emacs extension for using the codesearch
+;;; sourcecode indexer.
+;;
+;; Author: Austin Bingham <austin.bingham@gmail.com>
+;; Version: 1
+;; URL: https://github.com/abingham/codespeak.el
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; Copyright (c) 2012 Austin Bingham
+;;
+;;; Commentary:
+;;
+;; Description:
+;;
+;; This extension allows you to use the codesearch code indexing
+;; system in emacs.
+;;
+;; For more details, see the project page at
+;; https://github.com/abingham/prosjekt.
+;;
+;; For more details on codesearch, see its project page at
+;; http://code.google.com/p/codesearch/
+;;
+;; Installation:
+;;
+;; Copy codesearch.el to some location in your emacs load path. Then add
+;; "(require 'codesearch)" to your emacs initialization (.emacs,
+;; init.el, or something).
+;;
+;; Example config:
+;;
+;;   (require 'codesearch)
+;;
+;;; License:
+;;
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Code:
+
+
 (defcustom codesearch-csearch "csearch"
   "The name of the csearch program."
   :type '(string)
